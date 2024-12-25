e6210501690cd8dd8a379603514f51d9d586537e
Marcin Barć <marcin.barc.programowanie@gmail.com>
2024-12-25 20:02:54 +0100

    driversList with limit added to Car class

--- src/main/java/org/CEPiK/Car.java
+++ src/main/java/org/CEPiK/Car.java
@@ -1,11 +1,23 @@
 package org.CEPiK;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Car extends Vehicle {
+	private final List<CarDriver> driversList = new ArrayList<>();
+
 	public Car(final String model, final String vin, final String mark, final LocalDateTime productionDate,
 			   final String registrationNumber, final String technicalCondition, final String color) {
 		super(model, vin, mark, productionDate, registrationNumber, technicalCondition, color);
+	}
+
+	public boolean addDriver(final CarDriver carDriver) {
+		if (driversList.size() >= 2) {
+			return false;
+		}
+		driversList.add(carDriver);
+		return true;
 	}
 
 	@Override
