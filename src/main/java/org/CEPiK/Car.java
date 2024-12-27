1fcfb7a104c13c10c3fafe9a185fde16da05cb1c
Marcin Barć <marcin.barc.programowanie@gmail.com>
2024-12-27 19:41:08 +0100

    RegistrationCertificate added to repository

--- src/main/java/org/CEPiK/Car.java
+++ src/main/java/org/CEPiK/Car.java
@@ -1,13 +1,8 @@
 package org.CEPiK;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class Car extends Vehicle {
-	private final List<CarDriver> driversList = new ArrayList<>();
-
 	public Car(final String model, final String vin, final String mark, final LocalDateTime productionDate,
 			   final String registrationNumber, final String technicalCondition, final String color) {
 		super(model, vin, mark, productionDate, registrationNumber, technicalCondition, color);
@@ -15,31 +10,6 @@ public class Car extends Vehicle {
 
 	@Override
 	public String toString() {
-		return "Car{" +
-				"} " + super.toString();
-	}
-
-	@Override
-	public boolean equals(final Object o) {
-		if (!(o instanceof final Car car)) {
-			return false;
-		}
-		if (!super.equals(o)) {
-			return false;
-		}
-		return Objects.equals(driversList, car.driversList);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(super.hashCode(), driversList);
-	}
-
-	public boolean addDriver(final CarDriver carDriver) {
-		if (driversList.size() >= 2) {
-			return false;
-		}
-		driversList.add(carDriver);
-		return true;
+		return "Car{} " + super.toString();
 	}
 }
