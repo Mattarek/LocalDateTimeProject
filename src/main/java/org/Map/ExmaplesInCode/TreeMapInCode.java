59c9d702732ed102ef10b1d63555bebce221fbda
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-01-01 18:14:19 +0100

    TreeMapInCode added to repository

---
+++ src/main/java/org/Map/ExmaplesInCode/TreeMapInCode.java
@@ -0,0 +1,11 @@
+package org.Map.ExmaplesInCode;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ThreeMapInCode {
+	public static void main(final String[] args) {
+		// Tworzymy TreeMap, która przechowuje lementy w posortowanej kolejności według klucza
+		final Map<Integer, String> users = new TreeMap<>();
+	}
+}
