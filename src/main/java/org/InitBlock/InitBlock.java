e28d74f1751d4119b73264648e42b7163f6fa81f
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-01-04 21:42:35 +0100

    calling order

---
+++ src/main/java/org/InitBlock/InitBlock.java
@@ -0,0 +1,11 @@
+package org.InitBlock;
+
+public class InitBlock {
+	public static void main(final String[] args) {
+		System.out.println("Tworzymy obiekt klasy A:");
+		final A objectA = new A();
+
+		System.out.println("\nTworzymy obiekt klasy B:");
+		final B objectB = new B();
+	}
+}
