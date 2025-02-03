2b12dd4b77579e69be8a9b5dada2f2331e246d5f
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-02-03 22:16:56 +0100

    LocalDateTime with LocalDate and LocalTime

--- src/main/java/org/workingWithTime/LocalExamples.java
+++ src/main/java/org/workingWithTime/LocalExamples.java
@@ -8,7 +8,11 @@ import java.time.temporal.ChronoUnit;
 
 public class LocalExamples {
 	public static void main(final String[] args) {
-		final LocalDate id = LocalDate.of(2000, Month.MARCH, 20);
-		System.out.println(id.withMonth(Month.APRIL.ordinal() + 1)); // Aby wyświetlić Kwiecień, musimy dodać +1
+		final LocalDate localDate = LocalDate.of(2000, Month.MARCH, 20);
+		System.out.println(localDate.withMonth(Month.APRIL.ordinal() + 1)); // Aby wyświetlić Kwiecień, musimy dodać +1
+
+		final LocalTime localTime = LocalTime.parse("00:15:15");
+		final LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
+		System.out.println(localDateTime); // 2000-03-20T00:15:15
 	}
 }
