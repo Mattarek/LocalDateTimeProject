1975e3a727e777a80d6d9ab40a9262df6c5f8e80
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-02-03 21:19:25 +0100

    April from Month.MARCH

--- src/main/java/org/workingWithTime/LocalExamples.java
+++ src/main/java/org/workingWithTime/LocalExamples.java
@@ -3,11 +3,12 @@ package org.workingWithTime;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 
 public class LocalExamples {
 	public static void main(final String[] args) {
-		final LocalTime id = LocalTime.of(0, 10, 0, 24);
-		System.out.println(LocalTime.ofNanoOfDay(id.toNanoOfDay()));
-		System.out.println(LocalTime.ofSecondOfDay(id.toSecondOfDay()));
+		final LocalDate id = LocalDate.of(2000, Month.MARCH, 20);
+		System.out.println(id.withMonth(Month.APRIL.ordinal() + 1)); // Aby wyświetlić Kwiecień, musimy dodać +1
 	}
 }
