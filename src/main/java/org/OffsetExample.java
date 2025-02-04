3b00b0d05aabd975f654cef3ce5fd5a0d39b6a4b
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-02-04 18:00:23 +0100

    ZoneId with change time in Europe/Paris

--- src/main/java/org/OffsetExample.java
+++ src/main/java/org/OffsetExample.java
@@ -6,6 +6,7 @@ import java.time.LocalTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 public class OffsetExample {
 	public static void main(final String[] args) {
@@ -20,5 +21,20 @@ public class OffsetExample {
 		System.out.println("offsetDateTime.toLocalDateTime(): " + offsetDateTime.toLocalDateTime());
 		System.out.println("offsetDateTime.toZonedDateTime(): " + offsetDateTime.toZonedDateTime());
 		System.out.println("offsetDateTime.getOffset(): " + offsetDateTime.getOffset());
+
+		//============================================================
+		final ZoneId zone = ZoneId.of("Europe/Paris");
+		final LocalDate dateOne = LocalDate.of(2018, 10, 28);
+		final LocalTime timeOne = LocalTime.of(1, 55);
+		ZonedDateTime dateTime = ZonedDateTime.of(dateOne, timeOne, zone);
+		System.out.println(dateTime); // 2018-03-25T14:30+02:00[Europe/Paris]
+
+		//============================================================
+		// Wybierzmy date zmiany czasu na godzinę w tył, aby strefa sama zaktualizowała się za nas.
+		System.out.println("Step 1: " + dateTime); // Step 1: 2018-10-28T01:55+02:00[Europe/Paris]
+		dateTime = dateTime.plusHours(1);
+		System.out.println("Step 2: " + dateTime); // Step 2: 2018-10-28T02:55+02:00[Europe/Paris]
+		dateTime = dateTime.plusHours(2);
+		System.out.println("Step 3: " + dateTime); // Step 3: 2018-10-28T03:55+01:00[Europe/Paris]
 	}
 }
