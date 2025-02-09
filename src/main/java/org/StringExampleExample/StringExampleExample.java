921f141624569ae0a54612637b155a5204ae758d
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-02-09 22:00:10 +0100

    Supplier lambda in practice

--- src/main/java/org/StringExampleExample/StringExampleExample.java
+++ src/main/java/org/StringExampleExample/StringExampleExample.java
@@ -1,4 +1,31 @@
 package org.StringExampleExample;
 
+import java.time.LocalTime;
+import java.util.Random;
+import java.util.UUID;
+import java.util.function.Supplier;
+
 public class StringExampleExample {
+	public static void main(final String[] args) {
+		// Dodanie losowej liczby całkowitej
+		final int num = new Random().nextInt();
+		final Supplier<Integer> randomInt = () -> num;
+		System.out.println(randomInt.get());
+
+		// Dostarczenie aktualnej godziny
+		final Supplier<LocalTime> currentTime = LocalTime::now;
+		System.out.println(currentTime.get());
+
+		// Dostarczenie losowego identyfikatora UUID
+		final Supplier<String> randomUUID = () -> UUID.randomUUID().toString();
+		System.out.println(randomUUID.get());
+
+		// Dostarczenie domyślnego tekstu
+		final Supplier<String> defaultText = () -> "Hello, world";
+		System.out.println(defaultText.get());
+
+		// Dostarczanie losowej wartosci boolean
+		final Supplier<Boolean> randomBoolean = () -> new Random().nextBoolean();
+		System.out.println(randomBoolean.get());
+	}
 }
