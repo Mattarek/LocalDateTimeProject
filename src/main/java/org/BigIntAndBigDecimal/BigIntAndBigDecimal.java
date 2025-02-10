80c50d34c0875a2f0b9e6e78a98c77f193439383
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-02-10 23:54:02 +0100

    BigInteger method in practice

--- src/main/java/org/BigIntAndBigDecimal/BigIntAndBigDecimal.java
+++ src/main/java/org/BigIntAndBigDecimal/BigIntAndBigDecimal.java
@@ -4,17 +4,14 @@ import java.math.BigInteger;
 
 public class BigIntAndBigDecimal {
 	public static void main(final String[] args) {
-		final BigInteger bigIntegerZero = BigInteger.ZERO;
-		final BigInteger bigIntegerOne = BigInteger.ONE;
-		final BigInteger bigIntegerTwo = BigInteger.TWO;
-		final BigInteger bigIntegerTen = BigInteger.TEN;
+		final BigInteger a = new BigInteger("5000000000000000000000");
+		final BigInteger b = new BigInteger("2000000000000000000000");
 
-		System.out.println(bigIntegerZero);
-		System.out.println(bigIntegerTwo);
-		System.out.println(bigIntegerOne);
-		System.out.println(bigIntegerTen);
-
-		final BigInteger bigInteger = BigInteger.valueOf(Long.MAX_VALUE);
-		System.out.println(bigInteger);
+		System.out.println("Dodawanie: " + a.add(b));
+		System.out.println("Odejmowanie: " + a.subtract(b));
+		System.out.println("Mnożenie: " + a.multiply(b));
+		System.out.println("Dzielenie: " + a.divide(b));
+		System.out.println("Modulo: " + a.mod(b));
+		System.out.println("Potęgowanie: " + a.pow(2));
 	}
 }
