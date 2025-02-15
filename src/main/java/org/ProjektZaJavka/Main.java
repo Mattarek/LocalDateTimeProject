73588126519e2f8afea2059875760ebefea1807c
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-02-15 14:00:59 +0100

    InputData files ready to use

--- src/main/java/org/ProjektZaJavka/Main.java
+++ src/main/java/org/ProjektZaJavka/Main.java
@@ -1,7 +1,15 @@
 package org.ProjektZaJavka;
 
+import org.ProjektZaJavka.model.InputData;
+import org.staticInheritence.B;
+
+import java.math.BigDecimal;
+
 public class Main {
 	public static void main(final String[] args) {
-
+		final InputData inputData =
+				new InputData()
+						.withAmount(new BigDecimal("298000"))
+						.withMonthsDuration(BigDecimal.valueOf(160));
 	}
 }
--- src/main/java/org/ProjektZaJavka/model/InputData.java
+++ src/main/java/org/ProjektZaJavka/model/InputData.java
@@ -5,12 +5,42 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class InputData {
-	private final LocalDate repaymentStartDate = LocalDate.of(2020, 1, 6);
-	private final BigDecimal wiborPercent = new BigDecimal("1,73");
-	private final BigDecimal amount = new BigDecimal("300000");
-	private final BigDecimal monthsDuration = BigDecimal.valueOf(180);
-	private final RateType rateType = RateType.CONSTANT;
-	private final BigDecimal bankMargin = new BigDecimal("1.9");
+	private BigDecimal amount = new BigDecimal("300000");
+	private RateType rateType = RateType.CONSTANT;
+	private BigDecimal bankMargin = new BigDecimal("1.9");
+	private LocalDate repaymentStartDate = LocalDate.of(2020, 1, 6);
+	private BigDecimal wiborPercent = new BigDecimal("1,73");
+	private BigDecimal monthsDuration = BigDecimal.valueOf(180);
+
+	public InputData withRepaymentStartDate(final LocalDate repaymentStartDate) {
+		this.repaymentStartDate = repaymentStartDate;
+		return this;
+	}
+
+	public InputData withWiborPercent(final BigDecimal wiborPercent) {
+		this.wiborPercent = wiborPercent;
+		return this;
+	}
+
+	public InputData withMonthsDuration(final BigDecimal monthsDuration) {
+		this.monthsDuration = monthsDuration;
+		return this;
+	}
+
+	public InputData withRateType(final RateType rateType) {
+		this.rateType = rateType;
+		return this;
+	}
+
+	public InputData withBankMargin(final BigDecimal bankMargin) {
+		this.bankMargin = bankMargin;
+		return this;
+	}
+
+	public InputData withAmount(final BigDecimal amount) {
+		this.amount = amount;
+		return this;
+	}
 
 	public LocalDate getRepaymentStartDate() {
 		return repaymentStartDate;
