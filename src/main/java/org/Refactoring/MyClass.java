b5aa04fe4f32495091f27b7ff5a9a772d35de8c9
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-02-15 01:14:17 +0100

    Example for practice

---
+++ src/main/java/org/Refactoring/MyClass.java
@@ -0,0 +1,15 @@
+package org.Refactoring;
+
+import java.math.BigInteger;
+
+public class MyClass implements MyInterface {
+	@Override
+	public void method1(final String arg1, final BigInteger arg2) {
+
+	}
+
+	@Override
+	public void method1(final Integer arg1) {
+
+	}
+}
---
+++ src/main/java/org/Refactoring/MyInterface.java
@@ -0,0 +1,14 @@
+package org.Refactoring;
+
+import java.math.BigInteger;
+
+// ctrl + y - usunięcie lini
+// f2 - najedz na pierwszy bład
+// ctrl + d - zuplikuj linie
+public interface MyInterface {
+	void method1(final String arg1, final BigInteger arg2) {
+	}
+
+	void method1(final Integer arg1) {
+	}
+}
