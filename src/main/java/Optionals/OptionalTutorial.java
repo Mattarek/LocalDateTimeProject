859e8d53d65c669579772e5d1a851019765cf72e
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-02-26 19:40:45 +0100

    Optional with Cat

---
+++ src/main/java/Optionals/OptionalTutorial.java
@@ -0,0 +1,18 @@
+package Optionals;
+
+import java.util.Optional;
+
+public class OptionalTutorial {
+	public static void main(final String[] args) {
+		final Optional<Cat> myCat = findCatByName("Fred");
+		myCat.ifPresent(cat -> System.out.println(cat.getName()));
+		myCat.map(Cat::getAge).orElse(0);
+		System.out.println(myCat.get().getAge());
+
+		System.out.println(0);
+	}
+
+	private static Optional<Cat> findCatByName(final String name) {
+		return Optional.ofNullable(new Cat(name, 3));
+	}
+}
