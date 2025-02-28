061c80a7eff231e27a836f9655cc26c7bc586932
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-02-28 19:32:20 +0100

    This keyword

---
+++ src/main/java/This/Main.java
@@ -0,0 +1,15 @@
+package This;
+
+public class Main {
+	private String name;
+	private int age;
+
+	public static void main(final String[] args) {
+
+	}
+
+	public void setName(final String name, final int newAge) {
+		this.name = name;
+		age = newAge;
+	}
+}
