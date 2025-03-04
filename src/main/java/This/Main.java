43b8b0ec103af7595e7af161fc849618201dbbcf
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-03-04 17:31:18 +0100

    List stream().limit() test

--- src/main/java/This/Main.java
+++ src/main/java/This/Main.java
@@ -1,11 +1,15 @@
 package This;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	private String name;
 	private int age;
 
 	public static void main(final String[] args) {
-
+		final List testList = new ArrayList();
+		testList.stream().limit(3);
 	}
 
 	public void setName(final String name, final int newAge) {
