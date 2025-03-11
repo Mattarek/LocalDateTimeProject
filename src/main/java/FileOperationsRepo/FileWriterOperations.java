1ff4cae551c095bc4d4c6f7f52396ae6824681a9
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-03-11 16:06:59 +0100

    Another method with with Files and Paths

--- src/main/java/FileOperationsRepo/FileWriterOperations.java
+++ src/main/java/FileOperationsRepo/FileWriterOperations.java
@@ -1,4 +1,15 @@
 package FileOperationsRepo;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileWriterOperations {
+	public static void writeFile(final String fileName, final String content) {
+		try (final FileWriter writer = new FileWriter(fileName)) {
+			writer.write(content);
+			System.out.println("Dane zapisane do pliku za pomocą FileWriter.");
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/FileOperationsRepo/FilesOperations.java
+++ src/main/java/FileOperationsRepo/FilesOperations.java
@@ -1,5 +1,26 @@
 package FileOperationsRepo;
 
-public class FilesOperations {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+public class FilesOperations {
+	public static void writeFile(final String fileName, final String content) {
+		try {
+			Files.write(Paths.get(fileName), content.getBytes());
+			System.out.println("Dane zapisane do pliku za pomocą Files.");
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void readFile(final String fileName) {
+		try {
+			final List<String> lines = Files.readAllLines(Paths.get(fileName));
+			lines.forEach(System.out::println);
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
