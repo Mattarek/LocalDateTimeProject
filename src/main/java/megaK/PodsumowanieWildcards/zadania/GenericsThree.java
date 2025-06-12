75a0aea8d40538ce4449cc3464f930a85cbe4283
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-06-12 23:44:50 +0200

    Generics with Collection

--- src/main/java/megaK/PodsumowanieWildcards/zadania/GenericsThree.java
+++ src/main/java/megaK/PodsumowanieWildcards/zadania/GenericsThree.java
@@ -1,10 +1,32 @@
 package megaK.PodsumowanieWildcards.zadania;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
 public class GenericsThree {
 	public static void main(final String[] args) {
-
+		final Set<Integer> set = new HashSet<>(List.of(1, 2, 3, 5, 6, 11, 12));
+		final int result = collectionActionCounter(set, el -> el % 2 != 0);
+		System.out.println(result);
 	}
 
-	public static int collectionActionCounter() {
+	// 3. Napisz metodę generyczną, która zliczy ilość element w kolekcji, które mają w sobie cś charakterystycznego,
+	// np. liczby są nieparzyste, podane wyrazy sąpalindronami. Wykorzystaj predykat.
+	// Kolekcja elementów E
+	public static <T extends Collection<E>, E, F extends Predicate<E>> int collectionActionCounter(final T collection,
+																								   final F predicate
+	) {
+		int counter = 0;
+		// Skrót: iter
+		for (final E e : collection) {
+			if (predicate.test(e)) {
+				counter++;
+			}
+		}
+		return counter;
 	}
 }
