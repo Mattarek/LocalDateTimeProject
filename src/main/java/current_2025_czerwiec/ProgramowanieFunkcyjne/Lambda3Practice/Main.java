6e0b81bf5343d46baa02b6a2c215cf0501cf49eb
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-07-07 20:26:38 +0200

    Lambda is shit like that

--- src/main/java/current_2025_czerwiec/ProgramowanieFunkcyjne/Lambda3Practice/Main.java
+++ src/main/java/current_2025_czerwiec/ProgramowanieFunkcyjne/Lambda3Practice/Main.java
@@ -1,4 +1,18 @@
 package current_2025_czerwiec.ProgramowanieFunkcyjne.Lambda3Practice;
 
 public class Main {
+	public static void main(final String[] args) {
+		someStaticMethod(str -> System.out.println(str));
+	}
+
+	// Higher Order Function, bo ta funka przyjmuje inną funkcję
+	static IsSomeInterface someStaticMethod(final IsSomeInterface fun) {
+		fun.someMethod("String");
+		return other -> System.out.println("Returned: " + other);
+	}
+
+	@FunctionalInterface
+	interface IsSomeInterface {
+		void someMethod(String input);
+	}
 }
