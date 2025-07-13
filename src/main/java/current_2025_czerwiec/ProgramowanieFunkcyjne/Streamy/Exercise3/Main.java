f34d2d04a208f51e140f7ae315b0c6257c019fbc
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-07-13 22:28:28 +0200

    Stream: Distincs, we need to override equals and hashCode in our class

--- src/main/java/current_2025_czerwiec/ProgramowanieFunkcyjne/Streamy/Exercise3/Main.java
+++ src/main/java/current_2025_czerwiec/ProgramowanieFunkcyjne/Streamy/Exercise3/Main.java
@@ -3,7 +3,9 @@ package current_2025_czerwiec.ProgramowanieFunkcyjne.Streamy.Exercise3;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -52,24 +54,46 @@ public class Main {
 		//
 		//		System.out.println(stream);
 
-		// Part.4 Peek -
-		final List<Person> people = Arrays.asList(
-				new Person("Andrzej", new City("Warszawa")),
-				new Person("Tomek", new City("Gdańsk")),
-				new Person("Marek", new City("Łódź")),
-				new Person("Arek", new City("Szczecin")),
-				new Person("Olek", new City("Zakopane")),
-				new Person("Dagmara", new City("Zawierciewa"))
-		);
-		final List<Integer> collect = people.stream()
-				.peek(el -> System.out.println("Step1: " + el))
-				.map(Person::getCity)
-				.peek(c -> System.out.println("Step2: " + c))
-				.map(City::getName)
-				.peek(el -> System.out.println("Step3: " + el))
-				.map(String::length)
-				.toList();
-		System.out.println(collect);
+		//		//		 Part.4 Peek -
+		//				final List<Person> people = Arrays.asList(
+		//						new Person("Andrzej", new City("Warszawa")),
+		//						new Person("Tomek", new City("Gdańsk")),
+		//						new Person("Marek", new City("Łódź")),
+		//						new Person("Arek", new City("Szczecin")),
+		//						new Person("Olek", new City("Zakopane")),
+		//						new Person("Dagmara", new City("Zawierciewa"))
+		//				);
+		//				final List<Integer> collect = people.stream()
+		//						.peek(el -> System.out.println("Step1: " + el))
+		//						.map(Person::getCity)
+		//						.peek(c -> System.out.println("Step2: " + c))
+		//						.map(City::getName)
+		//						.peek(el -> System.out.println("Step3: " + el))
+		//						.map(String::length)
+		//						.toList();
+		//				System.out.println(collect);
+
+		//		// Distinct - służy do odsiania duplikatów
+		//		final List<City> cities = Arrays.asList(
+		//				new City("Warszawa"),
+		//				new City("Gdańsk"),
+		//				new City("Łódź"),
+		//				new City("Szczecin"),
+		//				new City("Szczecin"),
+		//				new City("Szczecin"),
+		//				new City("Zakopane"),
+		//				new City("Zawierciewa")
+		//		);
+		//
+		//		final String collect = cities.stream()
+		//				.distinct()
+		//				// usuwa dupliakty, działa na typy proste ale w przypadku new City(), porównuje
+		//				// referencje a te są różne, aby porównywac też typy złożone, musimy wygenerować equals i hashCode
+		//				.map(City::getName)
+		//				.collect(Collectors.joining(","));
+		//
+		//		System.out.println(collect);
+
 	}
 
 	static class Person {
@@ -99,6 +123,19 @@ public class Main {
 
 		public City(final String name) {
 			this.name = name;
+		}
+
+		@Override
+		public boolean equals(final Object o) {
+			if (!(o instanceof final City city)) {
+				return false;
+			}
+			return Objects.equals(name, city.name);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(name);
 		}
 
 		public String getName() {
