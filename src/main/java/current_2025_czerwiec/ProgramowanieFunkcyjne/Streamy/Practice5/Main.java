1678e0dce81eb8b0eac52ec32472ef13fbeee2c1
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-07-14 23:56:49 +0200

    Practice5, IntStream, DoubleStream, LongStream, int/double/long to average in practice

--- src/main/java/current_2025_czerwiec/ProgramowanieFunkcyjne/Streamy/Practice5/Main.java
+++ src/main/java/current_2025_czerwiec/ProgramowanieFunkcyjne/Streamy/Practice5/Main.java
@@ -1,4 +1,17 @@
 package current_2025_czerwiec.ProgramowanieFunkcyjne.Streamy.Practice5;
 
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
 public class Main {
+	public static void main(final String[] args) {
+		final Stream<String> stringStream = Stream.of("A", "AB", "ABC");
+		final IntStream intStream = stringStream.mapToInt(String::length);
+		intStream.forEach(System.out::println);
+
+		DoubleStream.empty().average();
+		LongStream.empty();
+	}
 }
