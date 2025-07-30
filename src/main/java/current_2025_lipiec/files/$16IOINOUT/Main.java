4ba709feeb52bdbf4cd3f4202b767fa187821984
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-07-30 23:12:51 +0200

    FileOutputStream with text vs photo

--- src/main/java/current_2025_lipiec/files/$16IOINOUT/Main.java
+++ src/main/java/current_2025_lipiec/files/$16IOINOUT/Main.java
@@ -8,16 +8,24 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.lang.reflect.Field;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(final String[] args) {
 		// tworzymy zmienną która przechowuje ścieżkę do naszego pliku
 		final File input = new File("src/main/java/current_2025_lipiec/files/$16IOINOUT/file.txt");
 		final File outputFile = new File("src/main/java/current_2025_lipiec/files/$16IOINOUT/file2.txt");
-		justCopyNoBuffer(input, outputFile);
+
+		final File inputPhoto = new File("src/main/java/current_2025_lipiec/files/$16IOINOUT/photo.png");
+		final File outputPhoto = new File("src/main/java/current_2025_lipiec/files/$16IOINOUT/photo2.png");
+		justCopyNoBuffer(input, outputFile, inputPhoto, outputPhoto);
+		final byte[] bytes = "ą".getBytes(StandardCharsets.UTF_8);
+		System.out.println(Arrays.toString(bytes)); // -60 -123
 	}
 
-	private static void justCopyNoBuffer(final File inputFile, final File outputFile) {
+	private static void justCopyNoBuffer(final File inputFile, final File outputFile, final File inputPhoto,
+										 final File outputPhoto) {
 		try (
 				// Czytamy i zapisujemy pojedynczo bajt po bajcie, bez bufferowania
 				final InputStream input = new FileInputStream(inputFile);
@@ -32,6 +40,20 @@ public class Main {
 				output.write(value);
 				value = input.read();
 				System.out.printf("Reading value: [%s], char [%s]%n", value, (char) value);
+			}
+		} catch (final Exception e) {
+			throw new RuntimeException(e);
+		}
+		try (
+				// Czytamy i zapisujemy pojedynczo bajt po bajcie, bez bufferowania
+				final InputStream input = new FileInputStream(inputPhoto);
+				final OutputStream output = new FileOutputStream(outputPhoto)
+		) {
+			int value = input.read();
+
+			while (value != -1) {
+				output.write(value);
+				value = input.read();
 			}
 		} catch (final Exception e) {
 			throw new RuntimeException(e);
