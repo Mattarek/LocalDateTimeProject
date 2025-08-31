88112425345267d069c1790ff099b4abd25d23e1
Marcin Barć <marcin.barc.programowanie@gmail.com>
2025-09-01 00:09:57 +0200

    JDBC Creact,Read,Delete

--- src/main/java/current_2025_sierpien/Projekt_Bazy/$2JDBC/DatabaseRunner.java
+++ src/main/java/current_2025_sierpien/Projekt_Bazy/$2JDBC/DatabaseRunner.java
@@ -3,8 +3,11 @@ package current_2025_sierpien.Projekt_Bazy.$2JDBC;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
@@ -17,7 +20,7 @@ public class DatabaseRunner {
 	private static final String SQL_INSERT
 			= "INSERT INTO TODOLIST (NAME, DESCRIPTION, DEADLINE, PRIORITY) VALUES (?,?,?,?)";
 	private static final String SQL_UPDATE
-			= "UPDATE TODOLIST SET DESCRIPTION = ?, DEADLLINE = ?, PRIORITY = ? WHERE NAME = ?;";
+			= "UPDATE TODOLIST SET DESCRIPTION = ?, DEADLINE = ?, PRIORITY = ? WHERE NAME = ?;";
 
 	private static final String SQL_READ_WHERE = "SELECT * FROM TODOLIST WHERE NAME = ?;";
 	private static final String SQL_READ_ALL = "SELECT * FROM TODOLIST;";
@@ -29,19 +32,76 @@ public class DatabaseRunner {
 
 	{
 		EXECUTION_MAP = Map.of(
-				Command.Type.CREATE,
-				this::runAdd
-		)
-		;
+				Command.Type.CREATE, this::runAdd,
+				Command.Type.UPDATE, this::runEdit,
+				Command.Type.DELETE_ALL, this::runDeleteAll,
+				Command.Type.READ, this::runRead
+		);
 	}
 
 	public void run(final Command command) {
 		System.out.println("##### RUNNING COMMAND #####");
 		final Consumer<Command> commandConsumer =
-				Optional.ofNullable(EXECUTION_MAP.get(command.getType())).orElseThrow(() -> new IllegalArgumentException(String.format("Command: [%s] not supported", command.getType())));
+				Optional.ofNullable(EXECUTION_MAP.get(
+						command.getType())).orElseThrow(
+						() -> new IllegalArgumentException(String.format("Command: [%s] not supported", command.getType())
+						));
 
 		commandConsumer.accept(command);
-		System.out.println("##### FINISHED COMMAND #####");
+		System.out.println("##### FINISHED COMMAND #####\n");
+	}
+
+	private void runRead(final Command command) {
+		if (!Command.Type.READ.equals(command.getType())) {
+			throw new IllegalArgumentException(command.getType().getName());
+		}
+
+		try (
+				final Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+				final PreparedStatement statement = connection.prepareStatement(SQL_READ_WHERE)
+		) {
+
+			try (final ResultSet resultSet = statement.executeQuery()) {
+				statement.setString(1, command.getToDoItem().getName());
+				final List<ToDoItem> readItems = mapMapToDoItem(resultSet);
+				print(readItems);
+				System.out.printf("Run: [%s] successfully, read: [%s] rows%n ", command.getType(), readItems.size());
+			}
+		} catch (final SQLException e) {
+			System.err.printf("[%s] data error. Message: [%s]%n", command.getType(), e.getMessage());
+		}
+	}
+
+	private void print(final List<ToDoItem> readItems) {
+		System.out.println("PRINTING TO DO LIST");
+		final String schema = "%-25s%-25s%-25s%-25s%n";
+		System.out.printf(schema,
+				ToDoItem.Field.NAME.name(),
+				ToDoItem.Field.DESCRIPTION.name(),
+				ToDoItem.Field.DEADLINE.name(),
+				ToDoItem.Field.PRIORITY.name()
+		);
+
+		readItems.forEach(entry -> System.out.printf(schema,
+				entry.getName(),
+				entry.getDescription(),
+				entry.getDeadline(),
+				entry.getPriority()
+		));
+	}
+
+	private List<ToDoItem> mapMapToDoItem(final ResultSet resultSet) throws SQLException {
+		final List<ToDoItem> result = new ArrayList<>();
+		while (resultSet.next()) {
+			final ToDoItem toDoItem = new ToDoItem();
+			toDoItem.setName(resultSet.getString(ToDoItem.Field.NAME.name()));
+			toDoItem.setDescription(resultSet.getString(ToDoItem.Field.DESCRIPTION.name()));
+			toDoItem.setDeadline(resultSet.getTimestamp(ToDoItem.Field.DEADLINE.name()).toLocalDateTime());
+			toDoItem.setPriority(resultSet.getInt(ToDoItem.Field.PRIORITY.name()));
+			result.add(toDoItem);
+		}
+
+		return result;
 	}
 
 	private void runAdd(final Command command) {
@@ -58,9 +118,45 @@ public class DatabaseRunner {
 			statement.setTimestamp(3, Timestamp.valueOf(command.getToDoItem().getDeadline()));
 			statement.setInt(4, command.getToDoItem().getPriority());
 			final int count = statement.executeUpdate();
-			System.out.printf("Run: [%s] successfully, inserted: [%s] rows%n ", command.getType(), count);
+			System.out.printf("Run: [%s] successfully, changed: [%s] rows%n ", command.getType(), count);
 		} catch (final SQLException e) {
-			System.err.printf("INSERT data error. Message: [%s]%n", e.getMessage());
+			System.err.printf("[%s] data error. Message: [%s]%n", command.getType(), e.getMessage());
+		}
+	}
+
+	private void runDeleteAll(final Command command) {
+		if (!Command.Type.DELETE_ALL.equals(command.getType())) {
+			throw new IllegalArgumentException(command.getType().getName());
+		}
+
+		try (
+				final Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+				final PreparedStatement statement = connection.prepareStatement(SQL_DELETE_ALL)
+		) {
+			final int count = statement.executeUpdate();
+			System.out.printf("Run: [%s] successfully, deleted: [%s] rows%n ", command.getType(), count);
+		} catch (final SQLException e) {
+			System.err.printf("[%s] data error. Message: [%s]%n", command.getType(), e.getMessage());
+		}
+	}
+
+	private void runEdit(final Command command) {
+		if (!Command.Type.UPDATE.equals(command.getType())) {
+			throw new IllegalArgumentException(command.getType().getName());
+		}
+
+		try (
+				final Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+				final PreparedStatement statement = connection.prepareStatement(SQL_UPDATE)
+		) {
+			statement.setString(1, command.getToDoItem().getDescription());
+			statement.setTimestamp(2, Timestamp.valueOf(command.getToDoItem().getDeadline()));
+			statement.setInt(3, command.getToDoItem().getPriority());
+			statement.setString(4, command.getToDoItem().getName());
+			final int count = statement.executeUpdate();
+			System.out.printf("Run: [%s] successfully, deleted: [%s] rows%n ", command.getType(), count);
+		} catch (final SQLException e) {
+			System.err.printf("[%s] data error. Message: [%s]%n", command.getType(), e.getMessage());
 		}
 	}
 }
--- src/main/java/current_2025_sierpien/Projekt_Bazy/$2JDBC/Main.java
+++ src/main/java/current_2025_sierpien/Projekt_Bazy/$2JDBC/Main.java
@@ -1,12 +1,12 @@
 package current_2025_sierpien.Projekt_Bazy.$2JDBC;
 
-import javax.xml.crypto.Data;
 import java.util.List;
 import java.util.Optional;
 
 public class Main {
 	static void main() {
 		final List<String> stringCommands = List.of(
+				"DELETE ALL;",
 				"CREATE;NAME=TASK1;DESCRIPTION=SOME DESCRIPTION1;DEADLINE=11.02.2021 20:10;PRIORITY=0",
 				"CREATE;NAME=TASK2;DESCRIPTION=SOME DESCRIPTION2;DEADLINE=12.02.2021 20:10;PRIORITY=1",
 				"CREATE;NAME=TASK3;DESCRIPTION=SOME DESCRIPTION3;DEADLINE=13.02.2021 20:10;PRIORITY=2",
